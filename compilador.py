9d9be4703787b606054b52b00ccdb79b78a9377c
Simei Thander <simeithander@gmail.com>
2019-02-18 09:31:47 -0300

    Update compilador.py
    Adicionado as instruções de R e J

--- compilador.py
+++ compilador.py
@@ -9,12 +9,31 @@ def convert_dec(n):
         total += (int(n[i]) * (2 ** i))
     return total
 
+#calcular o código address
+def calc_address(str):
+    cont_address = 1
+    cont_address_total = 0
+
+    for i in range(len(address_inver)):
+        if i != 0:
+            cont_address *= 2 
+        
+        if int(address_inver[i]) == 1:
+            cont_address_total += 1 * cont_address
+    return cont_address_total
+
 codigo_bin = input("Digite o codigo binário: ")
 op = convert_dec(codigo_bin[:6])
 rt = convert_dec(codigo_bin[11:16])
 rs =  convert_dec(codigo_bin[6:11])
+rd = convert_dec(codigo_bin[14:19])
 imm = convert_dec(codigo_bin[16:])
 func = convert_dec(codigo_bin[26:])
+address = codigo_bin[6:]
+address_inver = address[::-1]
+
+#calcular o código address
+
 
 if op == 32:
     #instrução l
@@ -23,12 +42,12 @@ if op == 32:
     else:
         print("lb {}$, {}(${})".format(rt, imm, rs))
 
-
 elif op == 0:
     #Operação do tipo R
-    print("OP: R")
+    if func == 34:
+        print("sub ${}, ${}, ${}".format(rd, rs, rt))
 
 elif op == 2:
     #Operação do tipo J
-    print("OP: J")
+    print("j {}".format(calc_address(address_inver)))
 
