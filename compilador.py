3c08114f9631d1d0b5222359128559b574bb1b2d
Simei Thander <simeithander@gmail.com>
2019-02-23 13:58:35 -0300

    Versão final
    - Adicionado leitura e escrita pelo TXT
    - Aprimorado o código.

--- compilador.py
+++ compilador.py
@@ -2,154 +2,169 @@
 Feito por Simei Thander - TADS 2018.2
 '''
 
+texto_saida = []
 
-def convert_dec(n):
-    n = (str(n)[::-1])
-    total = 0
-    for i in range(len(n)):
-        total += (int(n[i]) * (2 ** i))
-    return total
+#def para converter em decimal
+def convertdecimal(n):
+    n_invert = n[::-1]
+    cont_n = 1
+    cont_n_total = 0
 
-
-# calcular o código address
-def calc_address(str):
-    cont_address = 1
-    cont_address_total = 0
-
-    for i in range(len(address_inver)):
+    for i in range(len(n_invert)):
         if i != 0:
-            cont_address *= 2
+            cont_n *= 2
+        if int(n_invert[i]) == 1:
+            cont_n_total += 1 * cont_n
+    return cont_n_total
 
-        if int(address_inver[i]) == 1:
-            cont_address_total += 1 * cont_address
-    return cont_address_total
-
-
+#def para print da instrução
 def op_r(inst):
-    global rt, rs, rt
-    print("{} ${}, ${}, ${}".format(inst, rd, rs, rt))
-
+    global rt, rs, rt, texto_saida
+    texto_saida.append("{} ${}, ${}, ${} \n".format(inst, rd, rs, rt))
 
 def op_i(inst):
-    global rt, imm, rs
-    print("{} {}$, {}(${})".format(inst, rt, imm, rs))
+    global rt, imm, rs, texto_saida
+    texto_saida.append("{} {}$, {}(${}) \n".format(inst, rt, imm, rs))
 
-
-# leitura do codigo binário
-codigo_bin = input("Digite o codigo binário: ")
-
-# captura as variáveis e operações da String e converte em decimal
-op = convert_dec(codigo_bin[:6])
-rt = convert_dec(codigo_bin[11:16])
-rs = convert_dec(codigo_bin[6:11])
-rd = convert_dec(codigo_bin[14:19])
-imm = convert_dec(codigo_bin[16:])
-func = convert_dec(codigo_bin[26:])
-address = codigo_bin[6:]
-address_inver = address[::-1]
+def op_j(inst, address):
+    global texto_saida
+    texto_saida.append("{} {} \n".format(inst, address))
 
 # main code
 
-# Operação do tipo R
-if op == 0:
-    if func == 32:
-        op_r("add")
-    if func == 33:
-        op_r("addu")
-    if func == 34:
-        op_r("sub")
-    if func == 36:
-        op_r("and")
-    if func == 13:
-        op_r("break")
-    if func == 26:
-        op_r("div")
-    if func == 27:
-        op_r("divu")
-    if func == 9:
-        op_r("jalr")
-    if func == 8:
-        op_r("jr")
-    if func == 16:
-        op_r("mfhi")
-    if func == 18:
-        op_r("mflo")
-    if func == 24:
-        op_r("mult")
-    if func == 25:
-        op_r("multu")
-    if func == 19:
-        op_r("mtlo")
-    if func == 17:
-        op_r("mthi")
-    if func == 35:
-        op_r("subu")
-    if func == 36:
-        op_r("and")
-    if func == 37:
-        op_r("or")
-    if func == 38:
-        op_r("xor")
-    if func == 39:
-        op_r("nor")
-    if func == 42:
-        op_r("slt")
-    if func == 43:
-        op_r("sltu")
+# leitura do codigo binário no arquivo .txt 
+print("Lendo arquivo entrada.txt ...")
+arq = open('entrada.txt', 'r+')
+texto = arq.readlines()
 
-# Operação do tipo I
-if op == 32:
-    op_i("lb")
-if op == 33:
-    op_i("ih")
-if op == 8:
-    op_i("addi")
-if op == 9:
-    op_i("addiu")
-if op == 10:
-    op_i("slti")
-if op == 11:
-    op_i("sltiu")
-if op == 12:
-    op_i("andi")
-if op == 13:
-    op_i("ori")
-if op == 14:
-    op_i("xori")
-if op == 15:
-    op_i("lui")
-if op == 4:
-    op_i("beq")
-if op == 1 and rt == 1:
-    op_i("bgez")
-if op == 7:
-    op_i("bgtz")
-if op == 6:
-    op_i("blez")
-if op == 1:
-    op_i("bltz")
-if op == 5:
-    op_i("bne")
-if op == 36:
-    op_i("lbu")
-if op == 33:
-    op_i("lh")
-if op == 37:
-    op_i("lhu")
-if op == 35:
-    op_i("lw")
-if op == 49:
-    op_i("lwcl")
-if op == 40:
-    op_i("sb")
-if op == 41:
-    op_i("sh")
-if op == 43:
-    op_i("sw")
-if op == 57:
-    op_i("swcl")
+for linha in texto:
 
+    # captura as variáveis e operações da String e converte em decimal
+    op = convertdecimal(linha[:6].rstrip())
+    rt = convertdecimal(linha[11:16].rstrip())
+    rs = convertdecimal(linha[6:11].rstrip())
+    rd = convertdecimal(linha[14:19].rstrip())
+    imm = convertdecimal(linha[16:].rstrip())
+    func = convertdecimal(linha[26:].rstrip())
+    address = convertdecimal(linha[6:].rstrip())
 
-# Operação do tipo J
-if op == 2:
-    print("j {}".format(calc_address(address_inver)))
+    # Caso o OPcode for 0, ele chega as funções do operador R
+    if op == 0:
+        if func == 32:
+            op_r("add")
+        elif func == 0:
+            op_r("sll")
+        elif func == 33:
+            op_r("addu")
+        elif func == 34:
+            op_r("sub")
+        elif func == 36:
+            op_r("and")
+        elif func == 13:
+            op_r("break")
+        elif func == 26:
+            op_r("div")
+        elif func == 27:
+            op_r("divu")
+        elif func == 9:
+            op_r("jalr")
+        elif func == 8:
+            op_r("jr")
+        elif func == 16:
+            op_r("mfhi")
+        elif func == 18:
+            op_r("mflo")
+        elif func == 24:
+            op_r("mult")
+        elif func == 25:
+            op_r("multu")
+        elif func == 19:
+            op_r("mtlo")
+        elif func == 17:
+            op_r("mthi")
+        elif func == 35:
+            op_r("subu")
+        elif func == 36:
+            op_r("and")
+        elif func == 37:
+            op_r("or")
+        elif func == 38:
+            op_r("xor")
+        elif func == 39:
+            op_r("nor")
+        elif func == 42:
+            op_r("slt")
+        elif func == 43:
+            op_r("sltu")
+
+    # Caso as operações forem acima de 0, é obtido a instrução do tipo i
+    if op == 32:
+        op_i("lb")
+    elif op == 33:
+        op_i("ih")
+    elif op == 8:
+        op_i("addi")
+    elif op == 9:
+        op_i("addiu")
+    elif op == 10:
+        op_i("slti")
+    elif op == 11:
+        op_i("sltiu")
+    elif op == 12:
+        op_i("andi")
+    elif op == 13:
+        op_i("ori")
+    elif op == 14:
+        op_i("xori")
+    elif op == 15:
+        op_i("lui")
+    elif op == 4:
+        op_i("beq")
+    elif op == 1 and rt == 1:
+        op_i("bgez")
+    elif op == 7:
+        op_i("bgtz")
+    elif op == 6:
+        op_i("blez")
+    elif op == 1:
+        op_i("bltz")
+    elif op == 5:
+        op_i("bne")
+    elif op == 36:
+        op_i("lbu")
+    elif op == 33:
+        op_i("lh")
+    elif op == 37:
+        op_i("lhu")
+    elif op == 35:
+        op_i("lw")
+    elif op == 49:
+        op_i("lwcl")
+    elif op == 40:
+        op_i("sb")
+    elif op == 41:
+        op_i("sh")
+    elif op == 43:
+        op_i("sw")
+    elif op == 57:
+        op_i("swcl")
+
+    # Caso o Opcode for 2 e 3, é utilizado a instrução do tipo J
+    if op == 2:
+        op_j("j",address)
+    elif op == 3:
+        op_j("jal",address)
+
+#fecha a leitura do arquivo entrada.txt
+arq.close()
+
+print("Convertendo o código binário em Assembly ...")
+
+#Saida das instruções para saida.txt
+arq_saida = open('saida.txt', 'w')
+arq_saida.writelines(texto_saida)
+arq_saida.close()
+print("Arquivo saida.txt gerado")
+
+#apenas mantem o terminal aberto
+input()
