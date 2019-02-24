5a5810064ab40264cfea68c7cb988fd0b4ff198c
Simei Thander <simeithander@gmail.com>
2019-02-24 10:08:59 -0300

    Update outputs

--- compilador.py
+++ compilador.py
@@ -1,5 +1,5 @@
 '''
-Feito por Simei Thander - TADS 2018.2
+Feito por Simei Thander - TADS 2018.2 v2.0
 '''
 
 texto_saida = []
@@ -17,19 +17,6 @@ def convertdecimal(n):
             cont_n_total += 1 * cont_n
     return cont_n_total
 
-#def para print da instrução
-def op_r(inst):
-    global rt, rs, rt, texto_saida
-    texto_saida.append("{} ${}, ${}, ${} \n".format(inst, rd, rs, rt))
-
-def op_i(inst):
-    global rt, imm, rs, texto_saida
-    texto_saida.append("{} {}$, {}(${}) \n".format(inst, rt, imm, rs))
-
-def op_j(inst, address):
-    global texto_saida
-    texto_saida.append("{} {} \n".format(inst, address))
-
 # main code
 
 # leitura do codigo binário no arquivo .txt 
@@ -44,116 +31,131 @@ for linha in texto:
     rt = convertdecimal(linha[11:16].rstrip())
     rs = convertdecimal(linha[6:11].rstrip())
     rd = convertdecimal(linha[14:19].rstrip())
+    sa = convertdecimal(linha[20:25].rstrip())
     imm = convertdecimal(linha[16:].rstrip())
     func = convertdecimal(linha[26:].rstrip())
     address = convertdecimal(linha[6:].rstrip())
 
     # Caso o OPcode for 0, ele chega as funções do operador R
     if op == 0:
-        if func == 32:
-            op_r("add")
-        elif func == 0:
-            op_r("sll")
-        elif func == 33:
-            op_r("addu")
-        elif func == 34:
-            op_r("sub")
-        elif func == 36:
-            op_r("and")
-        elif func == 13:
-            op_r("break")
-        elif func == 26:
-            op_r("div")
-        elif func == 27:
-            op_r("divu")
-        elif func == 9:
-            op_r("jalr")
+        if func == 0:
+            texto_saida.append("sll ${}, ${}, ${} \n".format(rd, rs, rt))
+        elif func == 2:
+            texto_saida.append("srvl ${}, ${}, ${} \n".format(rd, rs, rt))
+        elif func == 3:
+            texto_saida.append("sra ${}, ${}, ${} \n".format(rd, rs, rt))
+        elif func == 4:
+            texto_saida.append("sllv ${}, ${}, ${} \n".format(rd, rs, rt))
+        elif func == 6:
+            texto_saida.append("srl ${}, ${}, ${} \n".format(rd, rs, sa))
+        elif func == 7:
+            texto_saida.append("srav ${}, ${}, ${} \n".format(rd, rs, rt))
         elif func == 8:
-            op_r("jr")
-        elif func == 16:
-            op_r("mfhi")
-        elif func == 18:
-            op_r("mflo")
-        elif func == 24:
-            op_r("mult")
-        elif func == 25:
-            op_r("multu")
-        elif func == 19:
-            op_r("mtlo")
-        elif func == 17:
-            op_r("mthi")
-        elif func == 35:
-            op_r("subu")
+            texto_saida.append("jr ${} \n".format(rs))
+        elif func == 9:
+            texto_saida.append("jalr ${}, ${} \n".format(rd, rs))  
+        elif func == 12:
+            texto_saida.append("syscall \n")
+        elif func == 32:
+            texto_saida.append("add ${}, ${}, ${} \n".format(rd, rs, rt))
+        elif func == 33:
+            texto_saida.append("addu ${}, ${}, ${} \n".format(rd, rs, rt))
+        elif func == 34:
+            texto_saida.append("sub ${}, ${}, ${} \n".format(rd, rs, rt))
         elif func == 36:
-            op_r("and")
+            texto_saida.append("addu ${}, ${}, ${} \n".format(rd, rs, rt))
+        elif func == 13:
+            texto_saida.append("break \n")
+        elif func == 26:
+            texto_saida.append("div ${}, ${} \n".format(rs, rt))
+        elif func == 27:
+            texto_saida.append("dvu ${}, ${} \n".format(rs, rt))     
+        elif func == 16:
+            texto_saida.append("mfhi ${} \n".format(rd))
+        elif func == 18:
+            texto_saida.append("mflo ${} \n".format(rd))
+        elif func == 24:
+            texto_saida.append("mult ${}, ${} \n".format(rs, rt))
+        elif func == 25:
+            texto_saida.append("multu ${}, ${} \n".format(rs, rt))
+        elif func == 19:
+            texto_saida.append("mtlo ${} \n".format(rs))
+        elif func == 17:
+            texto_saida.append("mthi ${} \n".format(rt))
+        elif func == 35:
+            texto_saida.append("subu ${}, ${}, ${} \n".format(rd, rs, rt))
+        elif func == 36:
+            texto_saida.append("and ${}, ${}, ${} \n".format(rd, rs, rt))
         elif func == 37:
-            op_r("or")
+            texto_saida.append("or ${}, ${}, ${} \n".format(rd, rs, rt))
         elif func == 38:
-            op_r("xor")
+            texto_saida.append("xor ${}, ${}, ${} \n".format(rd, rs, rt))
         elif func == 39:
-            op_r("nor")
+            texto_saida.append("nor ${}, ${}, ${} \n".format(rd, rs, rt))
         elif func == 42:
-            op_r("slt")
+            texto_saida.append("slt ${}, ${}, ${} \n".format(rd, rs, rt))
         elif func == 43:
-            op_r("sltu")
+            texto_saida.append("sltu ${}, ${}, ${} \n".format(rd, rs, rt))
+        elif func == 35:
+            texto_saida.append("subu ${}, ${}, ${} \n".format(rd, rs, rt))
 
     # Caso as operações forem acima de 0, é obtido a instrução do tipo i
     if op == 32:
-        op_i("lb")
+        texto_saida.append("lb {}(${}) \n".format(rt, imm))
     elif op == 33:
-        op_i("ih")
+        texto_saida.append("ih {}(${}) \n".format(rt, imm))
     elif op == 8:
-        op_i("addi")
+        texto_saida.append("addi ${}, ${}, {} \n".format(rt, rs, imm))
     elif op == 9:
-        op_i("addiu")
+        texto_saida.append("addiu ${}, ${}, {} \n".format(rt, rs, imm))
     elif op == 10:
-        op_i("slti")
+        texto_saida.append("slti ${}, ${}, {} \n".format(rt, rs, imm))
     elif op == 11:
-        op_i("sltiu")
+        texto_saida.append("sltiu ${}, ${}, {} \n".format(rt, rs, imm))
     elif op == 12:
-        op_i("andi")
+        texto_saida.append("andi ${}, ${}, {} \n".format(rt, rs, imm))
     elif op == 13:
-        op_i("ori")
+        texto_saida.append("ori ${}, ${}, {} \n".format(rt, rs, imm))
     elif op == 14:
-        op_i("xori")
+        texto_saida.append("xori ${}, ${}, {} \n".format(rt, rs, imm))
     elif op == 15:
-        op_i("lui")
+        texto_saida.append("lui ${}, {} \n".format(rt, imm))
     elif op == 4:
-        op_i("beq")
+        texto_saida.append("beq ${}, ${} \n".format(rs, rt))
     elif op == 1 and rt == 1:
-        op_i("bgez")
-    elif op == 7:
-        op_i("bgtz")
-    elif op == 6:
-        op_i("blez")
-    elif op == 1:
-        op_i("bltz")
+        texto_saida.append("bgez ${}\n".format(rs,))
+    elif op == 7 and rt == 0:
+        texto_saida.append("bgtz ${} \n".format(rs))
+    elif op == 6 and rt == 0:
+        texto_saida.append("blez ${} \n".format(rs))
+    elif op == 1 and rt == 0:
+        texto_saida.append("bltz ${} \n".format(rs))
     elif op == 5:
-        op_i("bne")
+        texto_saida.append("bne ${}, ${} \n".format(rs, rt))
     elif op == 36:
-        op_i("lbu")
+        texto_saida.append("lbu ${}, {}(${}) \n".format(rt, imm, rs))
     elif op == 33:
-        op_i("lh")
+        texto_saida.append("lh ${}, {}(${}) \n".format(rt, imm, rs))
     elif op == 37:
-        op_i("lhu")
+        texto_saida.append("lhu ${}, {}(${}) \n".format(rt, imm, rs))
     elif op == 35:
-        op_i("lw")
+        texto_saida.append("lw ${}, {}(${}) \n".format(rt, imm, rs))
     elif op == 49:
-        op_i("lwcl")
+        texto_saida.append("lwcl ${}, {}(${}) \n".format(rt, imm, rs))
     elif op == 40:
-        op_i("sb")
+        texto_saida.append("sb ${}, {}(${}) \n".format(rt, imm, rs))       
     elif op == 41:
-        op_i("sh")
+        texto_saida.append("sh ${}, {}(${}) \n".format(rt, imm, rs))
     elif op == 43:
-        op_i("sw")
+        texto_saida.append("sw ${}, {}(${}) \n".format(rt, imm, rs))
     elif op == 57:
-        op_i("swcl")
+        texto_saida.append("swcl ${}, {}(${}) \n".format(rt, imm, rs))
 
     # Caso o Opcode for 2 e 3, é utilizado a instrução do tipo J
     if op == 2:
-        op_j("j",address)
+        texto_saida.append("j {} \n".format(address))
     elif op == 3:
-        op_j("jal",address)
+        texto_saida.append("jal {} \n".format(address))
 
 #fecha a leitura do arquivo entrada.txt
 arq.close()
